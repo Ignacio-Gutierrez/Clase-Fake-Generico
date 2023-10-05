d2f79daa83233ba15606076c45a1398f69a402ac
Ignacio-Gutierrez <ij.gutierrez@alumno.edu.um.ar>
2023-10-04 22:34:34 -0300

    consultas funcionando

--- src/main/java/com/facu/restfake/services/ProductServiceImpl.java
+++ src/main/java/com/facu/restfake/services/ProductServiceImpl.java
@@ -7,15 +7,54 @@ import com.facu.restfake.repositories.ProductoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class ProductServiceImpl  extends BaseServiceImpl<Product, Long> implements ProductoService {
+public class ProductServiceImpl extends BaseServiceImpl<Product, Long> implements ProductoService {
 
     @Autowired
-    private ProductoRepository productoRepositoryRepository;
+    private ProductoRepository productoRepository;
 
-    public ProductServiceImpl(BaseRepository<Product, Long> baseRepository, ProductoRepository productoRepositoryRepository) {
+    public ProductServiceImpl(BaseRepository<Product, Long> baseRepository, ProductoRepository productoRepository) {
         super(baseRepository);
-        this.productoRepositoryRepository = productoRepositoryRepository;
+        this.productoRepository = productoRepository;
     }
 
+    @Override
+    public List<Product> buscarPorPrecioMayorA0(Double precioMinimo) throws Exception {
+        try {
+            List<Product> products = productoRepository.buscarPorPrecioMayorA0(precioMinimo);
+            return products;
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+    @Override
+    public List<Product> buscarPorPrecioMayorA1(Double precioMinimo) throws Exception {
+        try {
+            List<Product> products = productoRepository.buscarPorPrecioMayorA1(precioMinimo);
+            return products;
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    @Override
+    public List<Product> buscarPorPrecioEntre0(Double precioMinimo, Double precioMaximo) throws Exception {
+        try {
+            List<Product> products = productoRepository.buscarPorPrecioEntre0(precioMinimo, precioMaximo);
+            return products;
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+    @Override
+    public List<Product> buscarPorPrecioEntre1(Double precioMinimo, Double precioMaximo) throws Exception {
+        try {
+            List<Product> products = productoRepository.buscarPorPrecioEntre1(precioMinimo, precioMaximo);
+            return products;
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+    }
 }
